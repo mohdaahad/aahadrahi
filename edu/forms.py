8ccc462116f01b3e966f04df08e5fad29cda354a
aahadrahi <aahadrahi786@gmail.com>
2021-11-05 08:38:26 +0530

    work with http rediraction

--- edu/forms.py
+++ edu/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import widgets
 
-class StudentRegistration(forms.Form):
+class StudentRegistration(forms.Form ):
     name=forms.CharField()
     email=forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
